1038887ba09713ae6ccab4e44db9bdcdff7d5643
Alexey <167465433+h3avyof@users.noreply.github.com>
2024-12-24 15:16:16 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,8 +11,10 @@ function getRandomColor() {
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("switch");
   const h1 = document.getElementById("colorText_h1");
+  const h3 = document.getElementById("colorText_h3");
 
   button.addEventListener("click", () => {
     h1.style.color = getRandomColor();
+    h3.style.color = getRandomColor();
   });
 });
